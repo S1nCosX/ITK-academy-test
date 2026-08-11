7a7c0f5bbb57d065faa8d40b1778f81c1099e5ef
S1nCosX <andreyka_popov_2003@mail.ru>
2026-08-11 23:10:37 +0300

    Scripts: Fixed bash script

--- app.sh
+++ app.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-if [ -n "$1" ]
-then
-    if $1 == "--start"
-    then
-        if $2 == "-b" 
-        then
-        docker compose --env-file ./config.env up --build
+if [ -n "$1" ]; then
+    if [ $1 = "--start"]; then
+        if [ $2 = "-b" ]; then
+            docker compose --env-file ./config.env up --build
         else
             docker compose --env-file ./config.env up
-    else if $1 == "--restart"
-    then
-    docker compose down
-        if $2 == "-b" 
-        then
-        docker compose --env-file ./config.env up --build
+        fi
+    elif [ $1 = "--restart" ]; then
+        docker compose down
+        if [ $2 = "-b" ]; then
+            docker compose --env-file ./config.env up --build
         else
             docker compose --env-file ./config.env up
-    else if $1 == "--stop"
-    then
-    docker compose down
-else
+        fi
+    elif [ $1 = "--stop" ]; then
+        docker compose down
+    fi
+fi
